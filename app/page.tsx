22a41d80dc8dcc1097b518b0a9f617058f93b81a
O-K-G <55091190+O-K-G@users.noreply.github.com>
2026-07-02 22:41:55 -0400

    feat: send email (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Hero from "@components/hero/hero";
 import styles from "./page.module.css";
 import About from "@components/about/about";
 import Experience from "@components/experience/experience";
-
+// import { sendEmail } from "./utils/sendEmail";
 
 export default function Home() {
+
+  // TODO
+ //  sendEmail({from: 'test', subject: 'test1', text: 'xxx'}).then((x) => console.log(x))
   return (
     <main className={styles.mainContent}>
       <Hero />
