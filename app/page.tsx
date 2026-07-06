24823ce5fd537ad2de3c76751969769269f6aa8d
O. K. G. <inariwind@gmail.com>
2026-07-05 23:38:05 -0400

    refactor: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,8 @@ import Hero from "@components/hero/hero";
 import styles from "./page.module.css";
 import About from "@components/about/about";
 import Experience from "@components/experience/experience";
-// import { sendEmail } from "./utils/sendEmail";
 
 export default function Home() {
-
-  // TODO
- //  sendEmail({from: 'test', subject: 'test1', text: 'xxx'}).then((x) => console.log(x))
- // Email form: fonts + status message.
-
   return (
     <main className={styles.mainContent}>
       <Hero />
