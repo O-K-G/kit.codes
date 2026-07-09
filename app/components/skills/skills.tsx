aecd7072eb02a6a5930624bd0dd5caf574b9de62
O. K. G. <inariwind@gmail.com>
2026-07-08 23:09:53 -0400

    feat: skills

--- app/components/skills/skills.tsx
+++ app/components/skills/skills.tsx
@@ -2,9 +2,15 @@ import TopCardBar from "@ui/topCardBar/topCardBar";
 import styles from "./skills.module.css";
 import ContentSection from "@components/contentSection/contentSection";
 import Badge from "@ui/badge/badge";
-
-const EYEBROW = "4 · Skills";
-const TITLE = "Certificate of skill";
+import Typography from "@ui/typography/typography";
+import {
+  ADDITIONAL_DETAILS_LABEL,
+  ADDITIONAL_SKILLS,
+  EYEBROW,
+  SKILLS,
+  TITLE,
+  YEARS_LABEL,
+} from "./skills.constants";
 
 export default function Skills() {
   return (
@@ -17,6 +23,7 @@ export default function Skills() {
             <TopCardBar
               leftSlotColor="window"
               leftSlot={"Proficient in"}
+              className={styles.topBar}
               rightSlot={
                 <Badge
                   rotate="right"
@@ -27,6 +34,43 @@ export default function Skills() {
                 />
               }
             />
+
+            <ul className={styles.barsContainer}>
+              {SKILLS.map(({ skill, proficiency, years }) => {
+                const id = `skill-bar-${skill}`;
+                const yearsLabel = `${years} ${YEARS_LABEL}`;
+
+                return (
+                  <li key={id}>
+                    <Typography
+                      component="label"
+                      htmlFor={id}
+                      color="paper"
+                      variant="skill-label"
+                    >
+                      {skill}
+                    </Typography>
+
+                    <progress id={id} max={100} value={proficiency}>
+                      {proficiency}
+                    </progress>
+
+                    <Typography
+                      className="span"
+                      color="mist"
+                      variant="skill-label"
+                    >
+                      {yearsLabel}
+                    </Typography>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <Typography component="p" color="mist" variant="cert-also">
+              {ADDITIONAL_DETAILS_LABEL}&nbsp;
+              <strong>{ADDITIONAL_SKILLS}</strong>
+            </Typography>
           </div>
         </div>
       }
