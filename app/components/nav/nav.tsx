96445bce02066ae2834592f5ed5ff5ebc0c95fa1
O. K. G. <inariwind@gmail.com>
2026-07-10 22:55:33 -0400

    feat: nav - visual only

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,12 +1,45 @@
-import styles from './nav.module.css';
+import typographyStyles from "@ui/typography/typography.module.css";
+import styles from "./nav.module.css";
+import Link from "next/link";
+
+const LOGO = {
+  label: "kit.codes",
+  ariaLabel: "Homepage",
+};
 
 export default function Nav() {
   return (
-    <nav  className={styles.navigation}>
+    <nav className={styles.navigation}>
+      <header>
+        <Link
+          className={typographyStyles.typography}
+          data-variant="nav-logo"
+          aria-label={LOGO.ariaLabel}
+          href="#"
+        >
+          {LOGO.label}
+        </Link>
+      </header>
+
       <ul>
-        <li>n</li>
-        <li>n</li>
-        <li>n</li>
+        {[
+          { label: "G" },
+          { label: "1" },
+          { label: "2" },
+          { label: "3", selected: true },
+          { label: "4" },
+          { label: "R" },
+        ].map(({ label, selected }) => (
+          <li data-selected={selected} key={`nav-${label}}`}>
+            <Link
+              className={typographyStyles.typography}
+              data-variant="floor-btn"
+              href="#"
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
