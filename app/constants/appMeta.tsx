f066b78816205d2c3920db6dc6942ded838ce8c1
O. K. G. <inariwind@gmail.com>
2026-07-12 17:58:16 -0400

    refactor: meta

--- app/constants/appMeta.tsx
+++ app/constants/appMeta.tsx
@@ -2,5 +2,36 @@ import { EXPERIENCE_YEARS } from "../page.constants";
 
 export const APP_META = {
   title: "Kit G. — Web Developer",
-  description: `Portfolio of Kit G., a Web Developer with ${EXPERIENCE_YEARS} years building accessible React and Next.js applications for finance and tech.`,
+  description: `Landing page of Kit G., a Full-Stack Web Developer with ${EXPERIENCE_YEARS} years building accessible React and Next.js applications.`,
+  applicationName: "kit.codes",
+  manifest: "/manifest.webmanifest",
+  icons: {
+    icon: [
+      {
+        url: `/images/favicon-maple-code-32.png`,
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: `/images/favicon-maple-code-192.png`,
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: `/images/favicon-maple-code-512.png`,
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    shortcut: `/images/favicon-maple-code-32.png`,
+    apple: `/images/favicon-maple-code-180.png`,
+    other: [
+      {
+        rel: "mask-icon",
+        url: `/images/favicon-maple-code.svg`,
+        type: "image/svg+xml",
+        sizes: "any",
+      },
+    ],
+  },
 };
