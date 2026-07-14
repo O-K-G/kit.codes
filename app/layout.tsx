f77eb9ce67ca35d04ab95f3724491e7ca7385d5c
O. K. G. <inariwind@gmail.com>
2026-07-14 18:26:54 -0400

    feat: csp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { connection } from "next/server";
 import "./globals.css";
 import Nav from "@components/nav/nav";
 import { FONTS_VARIABLES } from "@utils/handleFonts";
@@ -6,11 +7,13 @@ import { APP_META } from "@constants/appMeta";
 
 export const metadata: Metadata = APP_META;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  await connection();
+
   return (
     <html lang="en" className={FONTS_VARIABLES}>
       <body>
