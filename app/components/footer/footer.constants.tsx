e764441db004bb5f547573441b0f7a112e5e6bfd
O-K-G <55091190+O-K-G@users.noreply.github.com>
2026-07-14 18:58:57 -0400

    refactor: footer (#22)

--- app/components/footer/footer.constants.tsx
+++ app/components/footer/footer.constants.tsx
@@ -1,10 +1,1 @@
-import { URLS } from "@/app/page.constants";
-import AnchorTag from "@/app/ui/anchorTag/anchorTag";
-
-export const LABEL = (
-  <>
-    Thanks for stopping by — building last renovated 2026, and you can check my
-    old deprecated site over&nbsp;
-    <AnchorTag href={URLS.oldSite}>here</AnchorTag>
-  </>
-);
+export const LABEL = "Thanks for stopping by — building last renovated 2026";
