2795b02c794551728232edfea34d5254ac009e83
O. K. G. <inariwind@gmail.com>
2026-07-14 18:41:19 -0400

    refactor: footer

--- app/components/footer/footer.constants.tsx
+++ app/components/footer/footer.constants.tsx
@@ -3,10 +3,8 @@ import AnchorTag from "@/app/ui/anchorTag/anchorTag";
 
 export const LABEL = (
   <>
-    Thanks for stopping by — building last renovated 2026
-    <span>
-      You can check my old deprecated site over&nbsp;
-      <AnchorTag href={URLS.oldSite}>here</AnchorTag>
-    </span>
+    Thanks for stopping by — building last renovated 2026, and you can check my
+    old deprecated site over&nbsp;
+    <AnchorTag href={URLS.oldSite}>here</AnchorTag>
   </>
 );
