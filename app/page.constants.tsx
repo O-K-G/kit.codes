dafaed57a62402e73b8b7cfe299c388ba79cf1a6
O. K. G. <inariwind@gmail.com>
2026-08-14 00:30:38 -0400

    refactor: cv

--- app/page.constants.tsx
+++ app/page.constants.tsx
@@ -15,5 +15,5 @@ export const URLS = {
   linkedIn: "https://linkedin.com/in/o-k-g",
   oldSite: 'https://devspace-center.vercel.app',
   resume:
-    "https://drive.google.com/file/d/1UncUL0oA933GNIb8owCAbIVqWJM_HGAs/view?usp=drive_link",
+    "https://drive.google.com/file/d/1qAXE-wdp4vD0Q59hdxdzFOmO3BZkazAY/view?usp=sharing",
 };
